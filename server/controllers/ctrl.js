6f4d336e7c6b39c054bfbc42ac65c5f7260dde86
FinoanaRandria <fitiavanarex4@gmail.com>
2023-04-25 20:03:59 +0300

    finish

--- server/controllers/ctrl.js
+++ server/controllers/ctrl.js
@@ -24,16 +24,40 @@ exports.home = (req,res)=>{
 
 //post 
 exports.postLogin = async (req,res)=>{
-     const data = req.body
-            console.log(data);
+     const emailPost = req.body.email
+     const passwordPost = req.body.password ;
+
+      console.log(passwordPost);
+      
     
-     const collect = await db.recuperer('name,password','')
-     
-        console.log(collect);
+     const collect = await db.recuperer('name,password','')          
+        //console.log(collect);
+      
+        const email = collect.map(elmt => elmt.name)
+        console.log(email);
+        console.log(emailPost);
+        const password = collect.map(elmt => elmt.password)
+         //filter
 
+         const filterEmail = email.filter(elmts =>{
+            return elmts == emailPost 
+         })
+      
+           const filterMdp = password.filter(elmts =>{
+                return elmts == passwordPost
+           })
+           console.log(filterEmail);
+           console.log(filterMdp);
+        if(emailPost == filterEmail[0] && passwordPost == filterMdp[0]){
+             
+              res.redirect('/home')
+                
+        }
 
-
-
-    res.end()
         
+        
+
+
+res.end();
+            
 }
